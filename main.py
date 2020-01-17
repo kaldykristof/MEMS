75c537337fea65838011ddb39112bc34ab7d0abd
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-01-17 14:15:11 +0100

    Adatbázis output

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
+import tkinter as tk
+#-SAJÁT-MODULOK-----------------------
 from lidar import object_in_range
 from database import Database
 
-if(object_in_range("COM3", 10, 30)):
-    print("Objektum a mérési határon belül!")
+FREE_PARKING_SPOTS = 50 # a parkolóház férőhelyeinek száma
+FREE_TIME = 10          # az ingyenesen bent tölthető idő mértéke
+TARIFF = 100            # egy időegység után fizetendő pénz mennyisége
 
-db = Database(host="remotemysql.com", username="NB8WskrWz5", password="iN23mdSang", database="NB8WskrWz5")
+if(object_in_range(port="COM3", angle=10, distance=30)):
+    pass # TODO: fotó készítése
+
+# TODO: karakterfelismerés a készített fotón
 
 car = "ABC-123"
 
-if (db.contains(car)):
-	# AUTÓ KIJÖN
-    time_delta = db.remove(car)
-	print("Bent töltött idő: {0}".format(time_delta))
-else:
-	# AUTÓ BEMEGY
-    db.insert(car)
+db = Database(host="remotemysql.com", username="NB8WskrWz5", password="iN23mdSang", database="NB8WskrWz5")
 
-db.close_connection()
+if (db.contains(car)):
+    # AUTÓ KIJÖN: bent töltött idő, fizetendő pénz kiszámítása, parkolóhely felszabadítása
+    time_delta = db.remove(car)
+    FREE_PARKING_SPOTS +=1
+    
+    payment = (time_delta - FREE_TIME) * TARIFF if (time_delta > FREE_TIME) else 0
+
+    window = tk.Tk()
+    window.title("Parkolóház")
+
+    tk.Label(window, text="Rendszám: {0}".format(car), font=(None, 50)).pack()
+    tk.Label(window, text="Bent töltött idő: {0} mp".format(time_delta), font=(None, 50)).pack()
+    tk.Label(window, text="Fizetendő: {0} Ft".format(payment), font=(None, 50)).pack()
+    tk.Button(window, text="Kilépés", command=window.destroy, height=10, width=50).pack()
+
+    window.mainloop()
+else:
+    # AUTÓ BEMEGY: rendszám, aktuális idő elmentése, parkolóhely lefoglalása
+    db.insert(car)
+    FREE_PARKING_SPOTS -= 1
+
+db.disconnect()
