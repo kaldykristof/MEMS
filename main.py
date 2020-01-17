a578604e9f23e598cee93844b44d157387c1f8ec
gboldi19 <gboldi19@freemail.hu>
2020-01-17 13:17:03 +0100

    Adatbázis modul hozzáadva

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 from lidar import object_in_range
+from database import Database
 
 if(object_in_range("COM3", 10, 30)):
     print("Objektum a mérési határon belül!")
+
+db = Database(host="remotemysql.com", username="NB8WskrWz5", password="iN23mdSang", database="NB8WskrWz5")
+
+car = "ABC-123"
+
+if (db.contains(car)):
+	# AUTÓ KIJÖN
+    time_delta = db.remove(car)
+	print("Bent töltött idő: {0}".format(time_delta))
+else:
+	# AUTÓ BEMEGY
+    db.insert(car)
+
+db.close_connection()
