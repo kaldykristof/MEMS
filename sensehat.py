f5193ca0b1fee55eb82382b736972d6a7b8f1ed9
kaldykristof <53399030+kaldykristof@users.noreply.github.com>
2020-01-22 12:53:03 +0100

    SenseHAT modul update

--- sensehat.py
+++ sensehat.py
@@ -1,77 +1,71 @@
 from time import sleep
 from sense_hat import SenseHat
 
-sense = SenseHat()
+red     = (255,   0,   0)
+green   = (  0, 255,   0)
+white   = (255, 255, 255)
+nothing = (  0,   0,   0)
 
-#clearing out panel
-sense.clear()
+R = red
+G = green
+_ = nothing
 
-#changes in orientation
-sense.set_rotation(270)
+check_mark = [
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, _, _,
+    _, _, _, _, _, _, G, _,
+    _, _, _, _, _, G, _, _,
+    _, _, _, _, G, _, _, _,
+    _, G, _, G, _, _, _, _,
+    _, _, G, _, _, _, _, _,
+    _, _, _, _, _, _, _, _
+]
 
-g = (0, 255, 0)
-hg = (0, 115, 0)
-hg2 = (0, 75, 0)
-b = (255, 0, 0)
-z = (0, 0, 0)
+x = [
+    _, _, _, _, _, _, _, _,
+    _, R, _, _, _, _, R, _,
+    _, _, R, _, _, R, _, _,
+    _, _, _, R, R, _, _, _,
+    _, _, _, R, R, _, _, _,
+    _, _, R, _, _, R, _, _,
+    _, R, _, _, _, _, R, _,
+    _, _, _, _, _, _, _, _
+]
 
-def validation_good():
-    #changes in orientation
-    sense.set_rotation(270)
-    sense.clear()
-    validation_good = [
-        z, z, z, z, z, z, z, z,
-        z, z, z, z, z, z, z, z,
-        z, z, z, z, z, z, g, z,
-        z, z, z, z, z, g, z, z,
-        z, z, z, z, g, z, z, z,
-        z, g, z, g, z, z, z, z,
-        z, z, g, z, z, z, z, z,
-        z, z, z, z, z, z, z, z
-        ]
-    sense.set_pixels(validation_good)
+rainbow = [
+    [255, 0, 0], [255, 0, 0], [255, 87, 0], [255, 196, 0], [205, 255, 0], [95, 255, 0], [0, 255, 13], [0, 255, 122],
+    [255, 0, 0], [255, 96, 0], [255, 205, 0], [196, 255, 0], [87, 255, 0], [0, 255, 22], [0, 255, 131], [0, 255, 240],
+    [255, 105, 0], [255, 214, 0], [187, 255, 0], [78, 255, 0], [0, 255, 30], [0, 255, 140], [0, 255, 248], [0, 152, 255],
+    [255, 223, 0], [178, 255, 0], [70, 255, 0], [0, 255, 40], [0, 255, 148], [0, 253, 255], [0, 144, 255], [0, 34, 255],
+    [170, 255, 0], [61, 255, 0], [0, 255, 48], [0, 255, 157], [0, 243, 255], [0, 134, 255], [0, 26, 255], [83, 0, 255],
+    [52, 255, 0], [0, 255, 57], [0, 255, 166], [0, 235, 255], [0, 126, 255], [0, 17, 255], [92, 0, 255], [201, 0, 255],
+    [0, 255, 66], [0, 255, 174], [0, 226, 255], [0, 117, 255], [0, 8, 255], [100, 0, 255], [210, 0, 255], [255, 0, 192],
+    [0, 255, 183], [0, 217, 255], [0, 109, 255], [0, 0, 255], [110, 0, 255], [218, 0, 255], [255, 0, 183], [255, 0, 74]
+]
 
-def validation_bad():
-    #changes in orientation
-    sense.set_rotation(270)
-    sense.clear()
-    validation_bad = [
-        z, z, z, z, z, z, z, z,
-        z, b, z, z, z, z, b, z,
-        z, z, b, z, z, b, z, z,
-        z, z, z, b, b, z, z, z,
-        z, z, z, b, b, z, z, z,
-        z, z, b, z, z, b, z, z,
-        z, b, z, z, z, z, b, z,
-        z, z, z, z, z, z, z, z
-        ]
-    sense.set_pixels(validation_bad)
+class MySenseHat(object):
+    def __init__(self):
+        self.sensehat = SenseHat()
+        self.sensehat.set_pixels(rainbow)
+        sleep(1)
+        self.sensehat.clear()
     
-def loading_anim():
-    #changes in orientation
-    sense.set_rotation(180)
-    sense.clear()
-    for j in range(1, 5):
-        for i in range(3,7):
-            sense.set_pixel(3, i-2, g)
-            sense.set_pixel(4, i-2, g)
-            sleep(0.4)
-            sense.set_pixel(3, i-1, hg)
-            sense.set_pixel(4, i-1, hg)
-            sleep(0.4)
-            sense.set_pixel(3, i, hg2)
-            sense.set_pixel(4, i, hg2)
-            sleep(0.4)
-        sense.clear()
-        
-print("Kérem az inputot: ")
-choice = input()
-if (choice == '1'):
-    validation_good()
-elif (choice == '2'):
-    validation_bad()
-elif (choice == '3'):
-    loading_anim()
-else:
-    print("Rossz input!")
-    sense.clear()
+    def clear(self):
+        self.sensehat.clear()
+    
+    def show_CM(self):
+        self.clear()
+        self.sensehat.set_pixels(check_mark)
+        sleep(1)
+        self.clear()
+
+    def show_X(self):
+        self.clear()
+        self.sensehat.set_pixels(x)
+        sleep(1)
+        self.clear()
+
+    def display(self, message):
+        self.clear()
+        self.sensehat.show_message(message, white)
+        self.clear()
